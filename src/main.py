8c15bf6ddfecc4c81e1007ce4c356a127f51cd23
Alcdies-Sorto <alsorto@gmail.com>
2023-06-19 11:56:41 -0700

    send message on start

--- src/main.py
+++ src/main.py
@@ -12,6 +12,8 @@ import grpc
 from farm_ng.canbus import canbus_pb2
 from farm_ng.canbus.packet import MotorState
 from farm_ng.service import service_pb2
+from farm_ng.canbus.packet import make_amiga_rpdo1_proto
+from farm_ng.canbus.packet import AmigaControlState
 
 # Must come before kivy imports
 os.environ["KIVY_NO_ARGS"] = "1"
@@ -87,7 +89,63 @@ class VirtualJoystickApp(App):
         # Placeholder task
         self.async_tasks.append(asyncio.ensure_future(self.template_function()))
 
+        self.async_tasks.append(
+            asyncio.ensure_future(self.send_can_msgs(canbus_client))
+        )
+
         return await asyncio.gather(run_wrapper(), *self.async_tasks)
+    
+    async def send_can_msgs(self, client: CanbusClient) -> None:
+        """This task ensures the canbus client sendCanbusMessage method has the pose_generator it will use to send
+        messages on the CAN bus to control the Amiga robot."""
+        while self.root is None:
+            await asyncio.sleep(0.01)
+
+        response_stream = None
+        while True:
+            # check the state of the service
+            state = await client.get_state()
+
+            # Wait for a running CAN bus service
+            if state.value != service_pb2.ServiceState.RUNNING:
+                # Cancel existing stream, if it exists
+                if response_stream is not None:
+                    response_stream.cancel()
+                    response_stream = None
+                print("Waiting for running canbus service...")
+                await asyncio.sleep(0.1)
+                continue
+
+            if response_stream is None and self.hidden_button:
+                print("Start sending CAN messages")
+                response_stream = client.stub.sendCanbusMessage(self.pose_generator())
+
+            try:
+                async for response in response_stream:
+                    # Sit in this loop and wait until canbus service reports back it is not sending
+                    assert response.success
+            except Exception as e:
+                print(e)
+                response_stream.cancel()
+                response_stream = None
+                continue
+
+            await asyncio.sleep(0.1)
+
+    async def pose_generator(self, period: float = 0.02):
+        """The pose generator yields an AmigaRpdo1 (auto control command) for the canbus client to send on the bus
+        at the specified period (recommended 50hz) based on the onscreen joystick position."""
+        while self.root is None:
+            await asyncio.sleep(0.01)
+
+        while True:
+            msg: canbus_pb2.RawCanbusMessage = make_amiga_rpdo1_proto(
+                state_req=AmigaControlState.STATE_AUTO_ACTIVE,
+                cmd_speed=self.max_speed,
+                cmd_ang_rate=self.max_angular_rate,
+            )
+            yield canbus_pb2.SendCanbusMessageRequest(message=msg)
+            await asyncio.sleep(period)
 
     async def template_function(self) -> None:
         """Placeholder forever loop."""
