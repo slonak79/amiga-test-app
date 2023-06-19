cca0e796dd70d8e0247a56c0d5f815025a00e689
Alcdies-Sorto <alsorto@gmail.com>
2023-06-19 11:45:24 -0700

    add default port

--- src/main.py
+++ src/main.py
@@ -112,7 +112,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--canbus-port",
         type=int,
-        required=True,
+        default=50060,
         help="The grpc port where the canbus service is running.",
     )
     # Add additional command line arguments here
