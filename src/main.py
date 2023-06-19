e4a3ac5c186ad84996a5254ab2b22787fffe54e2
Alcdies-Sorto <alsorto@gmail.com>
2023-06-19 10:04:37 -0700

    test message brain -> feather

--- src/main.py
+++ src/main.py
@@ -5,6 +5,13 @@ import os
 from typing import List
 from enum import Enum
 
+from farm_ng.canbus import canbus_pb2
+from farm_ng.canbus.canbus_client import CanbusClient
+from farm_ng.canbus.packet import AmigaControlState
+from farm_ng.canbus.packet import make_amiga_rpdo1_proto
+from farm_ng.service.service_client import ClientConfig
+from farm_ng.service import service_pb2
+
 # Must come before kivy imports
 os.environ["KIVY_NO_ARGS"] = "1"
 
@@ -41,6 +48,8 @@ class VirtualJoystickApp(App):
         super().__init__()
         self.hidden_button: bool = False
         self.async_tasks: List[asyncio.Task] = []
+        self.max_speed: float = 0.1
+        self.max_angular_rate: float = 0.1
 
     def build(self):
         return Builder.load_file("res/main.kv")
@@ -65,8 +74,17 @@ class VirtualJoystickApp(App):
             for task in self.async_tasks:
                 task.cancel()
 
+        # configure the canbus client
+        canbus_config: ClientConfig = ClientConfig(
+            address=self.address, port=self.canbus_port
+        )
+        canbus_client: CanbusClient = CanbusClient(canbus_config)
+
         # Placeholder task
         self.async_tasks.append(asyncio.ensure_future(self.template_function()))
+        self.async_tasks.append(
+            asyncio.ensure_future(self.send_can_msgs(canbus_client))
+        )
 
         return await asyncio.gather(run_wrapper(), *self.async_tasks)
 
@@ -76,12 +94,68 @@ class VirtualJoystickApp(App):
             await asyncio.sleep(0.01)
 
         while True:
-            await asyncio.sleep(0.5)
+            await asyncio.sleep(0.1)
 
-            # increment the counter using internal libs and update the gui
+            # update the gui
             self.root.ids.disable_button.disabled = self.hidden_button
 
+    async def send_can_msgs(self, client: CanbusClient) -> None:
+        """This task ensures the canbus client sendCanbusMessage method has the pose_generator it will use to send
+        messages on the CAN bus to control the Amiga robot."""
+        while self.root is None:
+            await asyncio.sleep(0.01)
 
+        response_stream = None
+        while True:
+            # check the state of the service
+            state = await client.get_state()
+
+            # Wait for a running CAN bus service
+            if state.value != service_pb2.ServiceState.RUNNING:
+                # Cancel existing stream, if it exists
+                if response_stream is not None:
+                    response_stream.cancel()
+                    response_stream = None
+                print("Waiting for running canbus service...")
+                await asyncio.sleep(0.1)
+                continue
+
+            if response_stream is None and self.hidden_button:
+                print("Start sending CAN messages")
+                # create my own app function to send message.
+                response_stream = client.stub.sendCanbusMessage(self.pose_generator())
+
+            try:
+                async for response in response_stream:
+                    # Sit in this loop and wait until canbus service reports back it is not sending
+                    assert response.success
+            except Exception as e:
+                print(e)
+                response_stream.cancel()
+                response_stream = None
+                continue
+
+            await asyncio.sleep(0.1)
+
+    async def pose_generator(self, period: float = 0.02):
+        """The pose generator yields an AmigaRpdo1 (auto control command) for the canbus client to send on the bus
+        at the specified period (recommended 50hz) based on the onscreen joystick position."""
+        while self.root is None:
+            await asyncio.sleep(0.01)
+
+        while True:
+            msg: canbus_pb2.RawCanbusMessage = make_amiga_rpdo1_proto(
+                state_req=AmigaControlState.STATE_AUTO_ACTIVE,
+                cmd_speed=self.max_speed,
+                cmd_ang_rate=self.max_angular_rate,
+            )
+            yield canbus_pb2.SendCanbusMessageRequest(message=msg)
+            await asyncio.sleep(period)
+
+
+# use wheel speed to test sending message.
+# when feather sees message turn LED to Green on state.
+# when no message turn LED Red to indicate off state.
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="joystick-tutorial")
 
